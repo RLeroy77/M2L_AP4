8201ff9ee5e047b9888b224eea0fa75ebc3475a9
Rémy Leroy <remy.leroy77@hotmail.fr>
2024-05-07 09:59:18 +0200

    changement BaseUrl

--- lib/Produits.dart
+++ lib/Produits.dart
@@ -7,7 +7,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Produits extends StatefulWidget {
   const Produits({Key? key}) : super(key: key);
 
-  static const String baseUrl = "http://10.0.2.2:8000";
+  static const String baseUrl = "http://10.74.1.151:8000";
 
   @override
   State<Produits> createState() => _ProduitsState();
